9af2f80e27b5f2c8bf70b657b04e23c5f530b460
cipo7741 <cipo7741@users.noreply.github.com>
2016-05-21 21:50:01 +0200

    Update guess.js

--- simple-guess/src/guess.js
+++ simple-guess/src/guess.js
@@ -1,5 +1,5 @@
 var xmlhttp = new XMLHttpRequest();
-var url = "data/java/lang/Number.json"
+var url = "data/java/lang/Number/Integer.json"
 var javaPackage = url.substring(5, url.length - 5).split("/").join(".")
 var method, jsonData;
 var numPoints, numQuests, numTries;
